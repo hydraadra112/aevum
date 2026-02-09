964cb04d55f4ca1815528582c46eb6f705c588b3
John Manuel Carado <carado.johnp11q@gmail.com>
2026-02-09 21:49:17 +0800

    feat: Add ProcessResult dataclass to prevent modifiability of results

--- src/aevum/base.py
+++ src/aevum/base.py
@@ -1,7 +1,16 @@
 from dataclasses import dataclass
-@dataclass
+
+@dataclass(frozen=True)
 class Process:
+    """The input data for a process."""
     pid: int
     burst_time: int
-    waiting_time: int | None = None
-    turnaround_time: int | None = None
+    arrival_time: int = 0
+
+@dataclass(frozen=True)
+class ProcessResult:
+    """The outcome of a process after simulation."""
+    process: Process
+    waiting_time: int
+    turnaround_time: int
+    completion_time: int
